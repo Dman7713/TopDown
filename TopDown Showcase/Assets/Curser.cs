282682b9f0596180d3e2a089f6690bec84f383f9
Dman7713 <PeytonPrivratsky@gmail.com>
2024-09-30 07:47:16 -0700

    added custom mouse

---
+++ TopDown Showcase/Assets/Curser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+
+    private Vector2 cursorHotspot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Correct way to create a Vector2
+        cursorHotspot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
+        // Correct spelling of Cursor
+        Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+    }
+}
